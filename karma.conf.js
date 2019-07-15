90578e513307c06560cd795e1c39db83a4286352
cumber <cumbermiao@163.com>
2019-07-15 09:55:52 +0800

    change karma config

--- karma.conf.js
+++ karma.conf.js
@@ -63,4 +63,19 @@ module.exports = function(config) {
 
     // plugins:['karma-coverage']
   });
+  var configuration = {
+    // other things
+ 
+    customLaunchers: {
+        Chrome_travis_ci: {
+            base: 'Chrome',
+            flags: ['--no-sandbox']
+        }
+    },
+  };
+ 
+  if (process.env.TRAVIS) {
+      configuration.browsers = ['Chrome_travis_ci'];
+  }
+  config.set(configuration);
 };
